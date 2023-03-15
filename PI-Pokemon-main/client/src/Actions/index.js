665160a9ced24fe5909839a96052cf2d0acf9422
MatiasSoto21 <matiassoto00@outlook.com>
2023-03-14 21:52:56 -0300

    front v1

--- PI-Pokemon-main/client/src/Actions/index.js
+++ PI-Pokemon-main/client/src/Actions/index.js
@@ -16,11 +16,11 @@ export const FAIL = 'FAIL';
 export const ORDER_HP = 'ORDER_HP';
 export const SPEED = 'SPEED';
 export const SPECIAL = 'SPECIAL';
-
+axios.defaults.baseURL = 'https://henry-pi-production-21a9.up.railway.app'
 
 export function getPokemons() {
     return async function (dispatch) {
-        return await axios("http://localhost:3001/pokemons")
+        return await axios("/pokemons")
             .then(res => dispatch({
                 type: GET_ALL_POKEMONS,
                 payload: res.data
@@ -30,7 +30,7 @@ export function getPokemons() {
 
 export function getPokemonName(name) {
     return async function (dispatch) {
-        return await axios("http://localhost:3001/pokemons?name=" + name)
+        return await axios("/pokemons?name=" + name)
             .then(res => dispatch({
                 type: GET_POKEMON_NAME,
                 payload: res.data
@@ -51,7 +51,7 @@ export function searchpokemon(payload) {
 
 export function getPokemonDetail(id) {
     return async function (dispatch) {
-        return await axios("http://localhost:3001/pokemons/" + id)
+        return await axios("/pokemons/" + id)
         .then(res => dispatch({
             type: GET_DETAIL,
             payload: res.data
@@ -61,7 +61,7 @@ export function getPokemonDetail(id) {
 
 export function getTypes() {
     return async function (dispatch) {
-        return await axios("http://localhost:3001/types")
+        return await axios("/types")
             .then(res => dispatch({
                 type: GET_TYPES,
                 payload: res.data
@@ -71,7 +71,7 @@ export function getTypes() {
 
 export function postPokemon(payload) {
     return async function (dispatch) {
-        return await axios.post("http://localhost:3001/pokemons", payload);
+        return await axios.post("/pokemons", payload);
     }
 }
 
