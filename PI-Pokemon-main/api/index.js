fd3d9b2d6d33c17d45ce5f57c4f6ba7078ef39ad
Matias Soto <matiassoto00@outlook.com>
2025-11-12 17:31:43 -0300

    Update server log message to include port number

--- PI-Pokemon-main/api/index.js
+++ PI-Pokemon-main/api/index.js
@@ -24,6 +24,6 @@ const port = process.env.PORT || 3001;
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(port, () => {
-    console.log('%s listening at server'); // eslint-disable-line no-console
+    console.log(`Server listening on port ${port}`); // eslint-disable-line no-console
   });
 });
